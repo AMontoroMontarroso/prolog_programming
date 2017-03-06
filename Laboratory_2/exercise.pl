95a65898362a1780578b96e3b53583145943c854
jcepedavillamayor <jcepedavillamayor@gmail.com>
2017-03-06 17:25:31 +0100

    Lab 2 completed

--- Laboratory_2/exercise.pl
+++ Laboratory_2/exercise.pl
@@ -16,3 +16,5 @@ memberSubstitute(X, [X|Tail], Tail).
 memberSubstitute(X, [Z|Tail], NewList) :- memberSubstitute(X, Tail, L2), append([Z], L2, NewList).
 
 descomponer(Val, A, B) :- between(0, Val, A), between(A, Val, B), Val is A + B.
+
+numBrandreth(N,C) :- between(32,99,N), between(1000,9999, C), L=C//100,R=mod(C,100),N*N =:= C, L+R=:=N.
